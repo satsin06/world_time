41809dc2c1138c0a38fb10f90a1d69d2d5292d15
satsin06 <satyamsinha9404@gmail.com>
2020-04-14 00:43:54 +0530

    Removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:worldtime/pages/choose_location.dart';
 import 'package:worldtime/pages/home.dart';
 import 'package:worldtime/pages/loading.dart';
 void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
   initialRoute: '/',
   routes: {
   '/': (context) => Loading(),
