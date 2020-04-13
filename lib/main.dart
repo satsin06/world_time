f43eeec795cb78f5e25fe10fc90c595ca038f336
satsin06 <satyamsinha9404@gmail.com>
2020-04-14 01:04:01 +0530

    no change

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:worldtime/pages/choose_location.dart';
 import 'package:worldtime/pages/home.dart';
 import 'package:worldtime/pages/loading.dart';
 void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
   initialRoute: '/',
   routes: {
   '/': (context) => Loading(),
